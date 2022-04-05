b1ef7a0cc8840eac31063e00be52c5e6297291e6
47Key <94882470+47Key@users.noreply.github.com>
2022-04-05 14:07:11 -0400

    Add files via upload

---
+++ database_fetch_post/fetch.js
@@ -0,0 +1,24 @@
+import fetch from "node-fetch";
+
+const url = "https://us-central1-key47-51636.cloudfunctions.net/app/login";
+const url2 = "https://us-central1-key47-51636.cloudfunctions.net/app/";
+const db = "https://key47-51636.firebaseio.com/.json";
+
+async function fetchDdf() {
+    const login = await fetch(url);
+    const response = await fetch(url2);
+    const data = await response.text();
+    const push = await fetch(db, {
+        method: "POST",
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log ("Success:", data);
+    })
+    .catch((error) => {
+    console.error("Error:", error);
+    });
+}
+
+fetchDdf();
