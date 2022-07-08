b2ec0dadd04df737fc9e21f1c1ceeee0b0f8875a
47key <rkeegan@live.com>
2022-07-08 15:52:14 -0400

    added data filtering for individualProp, and propGrid, into one file, to minimize overall file sizes and to make it easier to call one function instead of multiple

--- fetchPropDetails.js
+++ fetchPropDetails.js
@@ -2,7 +2,6 @@ import fetch from "node-fetch";
 
 var id = 1;
 const url = "https://us-central1-key47-51636.cloudfunctions.net/app/login";
-const db = "https://key47-51636.firebaseio.com/Properties.json";
 
 async function login() {
     const login = await fetch(url, {
@@ -41,10 +40,33 @@ async function search() {
         search();
     });
     const searchResponse = await searchId.text();
-    const db = `https://key47-51636.firebaseio.com/Properties/${id}.json`;
-    const put = await fetch(db, {
+    if (searchREsponse == null) {
+        search();
+    }
+
+    const parseData = JSON.parse(searchResponse);
+    const dataBase = parseData.records;
+
+    const cleanJson = {};
+
+////////////////////////////////////////////////////////////////
+/////////////////// Property Grid Details //////////////////////
+////////////////////////////////////////////////////////////////
+// Initializing basic json for property grid, to minimize the amount of data needed to be rendered on the website
+
+
+    cleanJson.photo = dataBase[0].Photo.PropertyPhoto[0].PhotoURL;
+    cleanJson.city = dataBase[0].Address.City;
+    cleanJson.price = dataBase[0].Price;
+    cleanJson.address = `${dataBase[0].Address.StreetNumber} ${dataBase[0].Address.StreetName}`
+    cleanJson.bedroomsTotal = dataBase[0].Building.BedroomsTotal;
+    cleanJson.bathroomsTotal = dataBase[0].Building.BathroomTotal;
+    cleanJson.listingId = dataBase[0].ListingID;
+
+    const dbPartial = `https://key47-51636.firebaseio.com/PropertyGrid/${id}.json`;
+    const put = await fetch(dbPartial, {
         method: "PUT",
-        body: JSON.parse(JSON.stringify(searchResponse)),
+        body: JSON.stringify(cleanJson),
     })
     .then(response => response.json())
     .then(data => {
@@ -53,8 +75,69 @@ async function search() {
     .catch((error) => {
     console.error("Error:", error);
     });
-    console.log(put);
-    return search();
+
+
+////////////////////////////////////////////////////////////////
+///////////////// Individual Property Details //////////////////
+////////////////////////////////////////////////////////////////
+// Initializing more advanced JSON to provide more detailed information about a property once that particular property is selected
+
+
+    cleanJson.description = dataBase[0].PublicRemarks;
+    cleanJson.buildingSqFt = dataBase[0].Building.TotalFinishedArea;
+    cleanJson.propertyType = dataBase[0].PropertyType;
+    cleanJson.buildingType = `${dataBase[0].Building.Type} ${dataBase[0].Building.ConstructionStyleAttachment}`;
+    cleanJson.stories = dataBase[0].Building.StoriesTotal;
+    cleanJson.title = dataBase[0].OwnershipType;
+    cleanJson.landSize = dataBase[0].Land.SizeTotalText;
+    cleanJson.buildDate = dataBase[0].Building.ConstructedDate;
+    cleanJson.parkingType = `${dataBase[0].ParkingSpaces.Parking[0].Name} ${dataBase[0].ParkingSpaces.Parking[1].Name}`;
+    cleanJson.latitude = dataBase[0].Address.Latitude;
+    cleanJson.longitude = dataBase[0].Address.Longitude;
+    cleanJson.appliances = dataBase[0].Building.Appliances;
+    cleanJson.flooring = dataBase[0].Building.FlooringType;
+    cleanJson.fireplace = dataBase[0].Building.FireplacePresent;
+    cleanJson.exteriorFinish = dataBase[0].Building.ExteriorFinish;
+    cleanJson.heatingType = `${dataBase[0].Building.HeatingType} ${dataBase[0].Building.HeatingFuel}`;
+    cleanJson.coolingType = dataBase[0].Building.CoolingType;
+    cleanJson.foundationType = dataBase[0].Building.FoundationType;
+    cleanJson.features = dataBase[0].Features;
+    cleanJson.pool = dataBase[0].PoolType;
+    cleanJson.zoning = dataBase[0].ZoningDescription;
+    cleanJson.mainPhoto = dataBase[0].Photo.PropertyPhoto[0].LargePhotoURL;
+
+    const filterOtherPhotos = dataBase[0].Photo.PropertyPhoto.map((photos) => photos.LargePhotoURL);
+    cleanJson.otherPhotos = filterOtherPhotos;
+
+    const dbFull = `https://key47-51636.firebaseio.com/IndividualProperty/${id}.json`;
+    const putFull = await fetch(dbFull, {
+        method: "PUT",
+        body: JSON.stringify(cleanJson),
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log ("Success:", data);
+    })
+    .catch((error) => {
+    console.error("Error:", error);
+    });
+
+
+// //////// OLD ///////
+//     const db = `https://key47-51636.firebaseio.com/Properties/${id}.json`;
+//     const put = await fetch(db, {
+//         method: "PUT",
+//         body: JSON.parse(JSON.stringify(searchResponse)),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//     console.log ("Success:", data);
+//     })
+//     .catch((error) => {
+//     console.error("Error:", error);
+//     });
+//     console.log(put);
+//     return search();
 }
 
 search();
