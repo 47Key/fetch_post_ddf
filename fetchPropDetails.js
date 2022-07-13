51e08c2b2a8179cb6f76ea4e4ef8af64afac1f12
47key <rkeegan@live.com>
2022-07-13 13:08:17 -0400

    :tada: Fixed bugs, still need a logout function. Added try+catch statements to handle errors, which scraps all of the property data if it's not available. Will need a better solution for that.

--- fetchPropDetails.js
+++ fetchPropDetails.js
@@ -36,7 +36,9 @@ const db = "https://teamforcier-default-rtdb.firebaseio.com/PropertyID.json";
 
 async function fetchDdf() {
     const response = await fetch(idUrl);
-    const data = await response.records.map(response => response._XmlAttributes.ID);
+    const preData = await response.json();
+    const data = preData.records.map(value => value._XmlAttributes.ID);
+    console.log(data);
     const push = await fetch(db, {
         method: "PUT",
         body: JSON.stringify(data),
@@ -60,11 +62,11 @@ fetchDdf();
 
 // Takes the ID's from the previous function, and fetches all of the data on that property
 
-var id = 1;
+var id = 66;
 
 async function search() {
     id++;
-    const response = await fetch(`https://teamforcier-default-rtdb.firebaseio.com/PropertyID/records/${id}/_XmlAttributes/ID.json`, {
+    const response = await fetch(`https://teamforcier-default-rtdb.firebaseio.com/PropertyID/${id}.json`, {
         method: "GET",
         mode: "cors",           
         cache: "force-cache",
@@ -105,76 +107,91 @@ async function search() {
 
     const cleanJson = {};
 
-    cleanJson.photo = dataBase[0].Photo.PropertyPhoto[0].PhotoURL;
-    cleanJson.city = dataBase[0].Address.City;
-    cleanJson.price = dataBase[0].Price;
-    cleanJson.address = `${dataBase[0].Address.StreetNumber} ${dataBase[0].Address.StreetName}`
-    cleanJson.bedroomsTotal = dataBase[0].Building.BedroomsTotal;
-    cleanJson.bathroomsTotal = dataBase[0].Building.BathroomTotal;
-    cleanJson.listingId = dataBase[0].ListingID;
-
-    const dbPartial = `https://teamforcier-default-rtdb.firebaseio.com/PropertyGrid/${id}.json`;
-    const put = await fetch(dbPartial, {
-        method: "PUT",
-        body: JSON.stringify(cleanJson),
-    })
-    .then(response => response.json())
-    .then(data => {
-    console.log ("Prop Grid Success:", data);
+    try { 
+        cleanJson.photo = dataBase[0].Photo.PropertyPhoto[0].PhotoURL;
+        cleanJson.city = dataBase[0].Address.City;
+        cleanJson.price = dataBase[0].Price;
+        cleanJson.address = `${dataBase[0].Address.StreetNumber} ${dataBase[0].Address.StreetName}`
+        cleanJson.bedroomsTotal = dataBase[0].Building.BedroomsTotal;
+        cleanJson.bathroomsTotal = dataBase[0].Building.BathroomTotal;
+        cleanJson.listingId = dataBase[0].ListingID;
+    } catch (error) {
+        return search();
+    }
+        
+        const dbPartial = `https://teamforcier-default-rtdb.firebaseio.com/PropertyGrid/${id}.json`;
+        const put = await fetch(dbPartial, {
+            method: "PUT",
+            body: JSON.stringify(cleanJson),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log ("Prop Grid Success:", data);
     })
     .catch((error) => {
     console.error("Prop Grid Error:", error);
     });
 
 
-////////////////////////////////////////////////////////////////
-///////////////// Individual Property Details //////////////////
-////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////
+// ///////////////// Individual Property Details //////////////////
+// ////////////////////////////////////////////////////////////////
 
 // Initializing more advanced JSON to provide more detailed information about a property once that particular property is selected
 // Continuation of Search function, but split into 2 sections for better organization
+    try {
+        cleanJson.description = dataBase[0].PublicRemarks;
+        cleanJson.buildingSqFt = dataBase[0].Building.TotalFinishedArea;
+        cleanJson.propertyType = dataBase[0].PropertyType;
+        cleanJson.buildingType = `${dataBase[0].Building.Type} ${dataBase[0].Building.ConstructionStyleAttachment}`;
+        cleanJson.stories = dataBase[0].Building.StoriesTotal;
+        cleanJson.title = dataBase[0].OwnershipType;
+        cleanJson.landSize = dataBase[0].Land.SizeTotalText;
+        cleanJson.buildDate = dataBase[0].Building.ConstructedDate;
+        cleanJson.parkingType = dataBase[0].ParkingSpaces.Parking;
+        cleanJson.latitude = dataBase[0].Address.Latitude;
+        cleanJson.longitude = dataBase[0].Address.Longitude;
+        cleanJson.appliances = dataBase[0].Building.Appliances;
+        cleanJson.flooring = dataBase[0].Building.FlooringType;
+        cleanJson.fireplace = dataBase[0].Building.FireplacePresent;
+        cleanJson.exteriorFinish = dataBase[0].Building.ExteriorFinish;
+        cleanJson.heatingType = `${dataBase[0].Building.HeatingType} ${dataBase[0].Building.HeatingFuel}`;
+        cleanJson.coolingType = dataBase[0].Building.CoolingType;
+        cleanJson.foundationType = dataBase[0].Building.FoundationType;
+        cleanJson.features = dataBase[0].Features;
+        cleanJson.pool = dataBase[0].PoolType;
+        cleanJson.zoning = dataBase[0].ZoningDescription;
+        cleanJson.mainPhoto = dataBase[0].Photo.PropertyPhoto[0].LargePhotoURL;
+        
+        const filterOtherPhotos = dataBase[0].Photo.PropertyPhoto.map((photos) => photos.LargePhotoURL); // filter LargePhotoUrl out of larger object, to minimize size of data rendered on the website
+        cleanJson.otherPhotos = filterOtherPhotos;
+    } catch (error) {
+        return search();
+    }
 
-    cleanJson.description = dataBase[0].PublicRemarks;
-    cleanJson.buildingSqFt = dataBase[0].Building.TotalFinishedArea;
-    cleanJson.propertyType = dataBase[0].PropertyType;
-    cleanJson.buildingType = `${dataBase[0].Building.Type} ${dataBase[0].Building.ConstructionStyleAttachment}`;
-    cleanJson.stories = dataBase[0].Building.StoriesTotal;
-    cleanJson.title = dataBase[0].OwnershipType;
-    cleanJson.landSize = dataBase[0].Land.SizeTotalText;
-    cleanJson.buildDate = dataBase[0].Building.ConstructedDate;
-    cleanJson.parkingType = `${dataBase[0].ParkingSpaces.Parking[0].Name} ${dataBase[0].ParkingSpaces.Parking[1].Name}`;
-    cleanJson.latitude = dataBase[0].Address.Latitude;
-    cleanJson.longitude = dataBase[0].Address.Longitude;
-    cleanJson.appliances = dataBase[0].Building.Appliances;
-    cleanJson.flooring = dataBase[0].Building.FlooringType;
-    cleanJson.fireplace = dataBase[0].Building.FireplacePresent;
-    cleanJson.exteriorFinish = dataBase[0].Building.ExteriorFinish;
-    cleanJson.heatingType = `${dataBase[0].Building.HeatingType} ${dataBase[0].Building.HeatingFuel}`;
-    cleanJson.coolingType = dataBase[0].Building.CoolingType;
-    cleanJson.foundationType = dataBase[0].Building.FoundationType;
-    cleanJson.features = dataBase[0].Features;
-    cleanJson.pool = dataBase[0].PoolType;
-    cleanJson.zoning = dataBase[0].ZoningDescription;
-    cleanJson.mainPhoto = dataBase[0].Photo.PropertyPhoto[0].LargePhotoURL;
-
-    const filterOtherPhotos = dataBase[0].Photo.PropertyPhoto.map((photos) => photos.LargePhotoURL); // filter LargePhotoUrl out of larger object, to minimize size of data rendered on the website
-    cleanJson.otherPhotos = filterOtherPhotos;
-
-    const dbFull = `https://teamforcier-default-rtdb.firebaseio.com/IndividualProperty/${id}.json`;
-    const putFull = await fetch(dbFull, {
-        method: "PUT",
-        body: JSON.stringify(cleanJson),
-    })
-    .then(response => response.json())
-    .then(data => {
-    console.log ("Ind Prop Success:", data);
+        const dbFull = `https://teamforcier-default-rtdb.firebaseio.com/IndividualProperty/${id}.json`;
+        const putFull = await fetch(dbFull, {
+            method: "PUT",
+            body: JSON.stringify(cleanJson),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log ("Ind Prop Success:", data);
     })
     .catch((error) => {
     console.error("Ind Prop Error:", error);
     });
+    return search();
+}
 
+search();
 
-// //////// OLD ///////
+/// D E P R E C A T E D ////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+///////////////////// D E P R E C A T E D //////////////////////
+////////////////////////////////////////////////////////////////
+//////////////////////////////////////// D E P R E C A T E D ///
+
 //     const db = `https://key47-51636.firebaseio.com/Properties/${id}.json`;
 //     const put = await fetch(db, {
 //         method: "PUT",
@@ -189,6 +206,9 @@ async function search() {
 //     });
 //     console.log(put);
 //     return search();
-}
 
-search();
+/// D E P R E C A T E D ////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+///////////////////// D E P R E C A T E D //////////////////////
+////////////////////////////////////////////////////////////////
+//////////////////////////////////////// D E P R E C A T E D ///
